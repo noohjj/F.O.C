3042d0f1c065c84710e50aca4860f916280023e4
noohjj <yk3216@naver.com>
2024-12-06 12:57:37 +0900

    focfinal

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -18,11 +18,17 @@ const GoText = styled.div`
   font-weight: 900;
   letter-spacing: 2px;
 `;
+
+const Title = styled.h3`
+  margin-top: 20px;
+  font-size: 20px;
+`;
 const Home = () => {
   return (
     <Wrap>
       <PageTitle title="Home" />
       <Logos />
+      <Title>~ 기록으로 도전하는 암산게임! ~</Title>
       <Link to="/gamehome">
         <GoText>LET'S START!</GoText>
       </Link>
